034ba4653f93ef731955c9b5fbcbbfbe00dbbaf6
Lawrence <lawrencecaoili10@gmail.com>
2026-08-25 13:52:09 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
   // Paste the /exec URL of your deployed Apps Script Web App here.
   // Deploy > Manage deployments > Web app > copy the URL ending in /exec
 
-  var APPS_SCRIPT_URL = 'PASTE_YOUR_WEB_APP_EXEC_URL_HERE';
+  var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxrY8PAtR23fd_Sj--KUpcGmRbzUxLfwZUB1S0cMGN6ixhwOf-b8ng_qCBaZU6f8_E8hg/exec';
 
   // ============================================================
   // DOM REFERENCES
@@ -881,7 +881,7 @@
 
     if (
       !APPS_SCRIPT_URL ||
-      APPS_SCRIPT_URL.indexOf('https://script.google.com/macros/library/d/131RA5UUg9EFrs3ktC5F5YVP1ysbg8eZdvlpUsjMZVI6HChzBV3wQ13JA/4') === 0
+      APPS_SCRIPT_URL.indexOf('PASTE_YOUR') === 0
     ) {
 
       statusMsg.textContent =
